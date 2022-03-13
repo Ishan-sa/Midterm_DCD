ad6192a12a3093906fd475c38e84a1f32271c644
alexsomerville <asomerville1@my.bcit.ca>
2022-03-13 15:44:25 -0700

    attempted the pop ups, got one to sort of work

--- JS/background-3.js
+++ JS/background-3.js
@@ -25,20 +25,20 @@ template_background3.innerHTML = `
         display: none;
    }
    
-    .back-div{
+/* had to change the styling to work on my computer, hopefully doesn't mess up on yours */
+
+   .back-div{
+    position: absolute;
+    margin-top: -793px;
+    margin-left: -347px;
+    }
+
+    .home-div{
         position: absolute;
         top: 22px;
         right: 22px;
-    }
+      }
 
-    .back-div{
-        position: absolute;
-        display: flex;
-        align-items: right;
-        margin-left: 314px;
-        margin-top: -548px;
-    }
-    
     /* Back Button */
 
 div.arrow {
@@ -51,6 +51,7 @@ div.arrow {
     transform: rotate(135deg);
     rotate: 90deg;
 }
+
 div.arrow::before {
     content: '';
     width: 100%;
@@ -62,6 +63,7 @@ div.arrow::before {
     display: block;
     transform-origin: 100% 0;
 }
+
 div.arrow:after {
     content: '';
     float: left;
@@ -75,11 +77,13 @@ div.arrow:after {
     transform-origin: 100% 0;
     transition: 0.2s ease;
 }
+
 div.arrow:hover::after {
     transform: rotate(45deg);
     border-color: #3dcfff;
     height: 120%;
 }
+
 div.arrow:hover::before {
     border-color: #3dcfff;
     transform: scale(0.8);
@@ -93,7 +97,14 @@ div.arrow:hover::before {
     display: none;
 }
 
+<!--- created seperate pop ups for each different svg, 
+probably not the best way to do it. --->
+
 .popup{
+    display: none;
+}
+
+#popup_container1{
     background: rgba(0, 0, 0, 0.6);
     width: 375px;
     height: 812px;
@@ -106,12 +117,30 @@ div.arrow:hover::before {
     display: none;
 }
 
-.popup .show{
+#popup_container2{
+    background: rgba(0, 0, 0, 0.6);
+    width: 375px;
+    height: 812px;
+    border-radius: 15px;
+    position: absolute;
+    top: 0;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    display: none;
 }
 
-.popup-content{
-
+#popup_container3{
+    background: rgba(0, 0, 0, 0.6);
+    width: 375px;
+    height: 812px;
+    border-radius: 15px;
+    position: absolute;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: none;
 }
 
 .test{
@@ -138,9 +167,9 @@ div.arrow:hover::before {
 <div class="content">
     <head-card icon="./Assets/warning.svg" text_uwc="Unsafe Working Conditions" class="hc"></head-card>
 
-    <head-card icon="./Assets/watericon.svg" text_uwc="Water Waste" class="hc"></head-card>
+    <head-card icon="./Assets/watericon.svg" text_uwc="Water Waste" class="hc2"></head-card>
 
-    <head-card icon="./Assets/Pollution-icon.svg" text_uwc="Pollution" class="hc"></head-card>
+    <head-card icon="./Assets/Pollution-icon.svg" text_uwc="Pollution" class="hc3"></head-card>
 </div>
 
 
@@ -150,10 +179,27 @@ div.arrow:hover::before {
     <img class="unsafe" src="./Assets/uwc.svg" alt="">
 </div>
 
-<div class="popup" id="popup_container">
-    <div class="popup-content">
-        <img class="unsafe" src="./Assets/uwc.svg" alt="">
+<!--- created seperate pop ups for each different svg, 
+probably not the best way to do it. --->
+
+<div class="popup">
+    <div class="popup1" id="popup_container1">
+        <div class="popup-content">
+            <img class="unsafe" src="./Assets/uwc.svg" alt="">
+        </div>
     </div>
+
+    <div class="popup2" id="popup_container2">
+        <div class="popup-content">
+            <img class="unsafe" src="./Assets/ww2.svg" alt="">
+        </div>
+    </div>
+
+    <div class="popup3" id="popup_container3">
+        <div class="popup-content">
+            <img class="unsafe" src="./Assets/Pol.svg" alt="">
+        </div>
+</div>
 </div>
 
 <!--- <button class="test" id="open">
@@ -176,12 +222,32 @@ class TheBackground3 extends HTMLElement {
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
     connectedCallback(){
         this.shadowRoot.appendChild(template_background3.content.cloneNode(true)); //use the template to make a clone
-
-    
+        this.shadowRoot.querySelector('.hc').onclick = () => this.PopUp(); 
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
+    PopUp(text_uwc = "Unsafe Working Conditions") {
+        
+        if (text_uwc = "Unsafe Working Conditions") {
+            this.shadowRoot.querySelector('#popup_container1').style.cssText = `
+            display: flex; 
+        `}
+
+        if (text_uwc = "Water Waste") {
+            this.shadowRoot.querySelector('#popup_container2').style.cssText = `
+            display: flex; 
+            `}
+        
+        if (text_uwc = "Pollution") {
+            this.shadowRoot.querySelector('#popup_container3').style.cssText = `
+            display: flex; 
+            `}
     }
+
+    }
+
+    
+
 
 
     // extras
